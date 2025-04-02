382e3b44de9e297ddc449e471b2d30617fe6af0b
Nour Sadek <nour.sadek@mail.utoronto.ca>
2025-04-02 17:15:16 -0400

    changed where certian properties are calculated

--- decision_tree_node.py
+++ decision_tree_node.py
@@ -8,13 +8,13 @@ class DecisionTreeNode:
 
     _feature: str
     _splitting_criteria: float
-    _samples: list[int]
+    _samples: np.ndarray
     _num_samples: int
     _depth: int
     _right: "DecisionTreeNode"
     _left: "DecisionTreeNode"
 
-    def __init__(self, depth: int, samples: list[int]) -> None:
+    def __init__(self, depth: int, samples: np.ndarray) -> None:
         """Initialize a new <DecisionTreeNode> as a leaf first. Since it is a leaf, it doesn't have right or left nodes,
         and it does not represent a feature from which a branching decision is made.
         <samples> represents a list of indices from the original training dataset that this node contains.
@@ -25,7 +25,7 @@ class DecisionTreeNode:
         self._left = None
         self._splitting_criteria = None
         self._depth = depth
-        self._samples = [sample for sample in samples]
+        self._samples = samples
         self._num_samples = len(samples)
 
     @property
@@ -49,7 +49,7 @@ class DecisionTreeNode:
         return self._left
 
     @property
-    def samples(self) -> list[int]:
+    def samples(self) -> np.ndarray:
         return self._samples
 
 
@@ -61,7 +61,7 @@ class RegressionNode(DecisionTreeNode):
     _prediction: float
     _sum_squared_residuals: float
 
-    def __int__(self, depth: int, samples: list[int], y: np.ndarray) -> None:
+    def __int__(self, depth: int, samples: np.ndarray) -> None:
         """Initialize a <DecisionTreeNode> specifically to be used in Regression Decision Trees.
         <y> are the target variables for the observations of the samples at the indices determined by <samples>. <y> is
         used to determine the <self._prediction>, which is the average of the values in <y>, as well as the
@@ -69,12 +69,8 @@ class RegressionNode(DecisionTreeNode):
         in <y>) and the predicted (average of the values in <y>).
         """
         super().__init__(depth, samples)
-        # Calculate the predicted output of this node
-        self._prediction = float(np.average(y))
-        # Calculate the sum of squared residuals of this node
-        self._sum_squared_residuals = 0
-        for observed in y:
-            self._sum_squared_residuals = self._sum_squared_residuals + pow((observed - self._prediction), 2)
+        self._prediction = None
+        self._sum_squared_residuals = None
 
     @property
     def prediction(self) -> float:
@@ -84,6 +80,17 @@ class RegressionNode(DecisionTreeNode):
     def sum_squared_residuals(self) -> float:
         return self._sum_squared_residuals
 
+    def set_properties(self, sum_squared_residuals: float, prediction: float) -> None:
+        self._prediction = prediction
+        self._sum_squared_residuals = sum_squared_residuals
+
+    def determine_sum_squared_residuals_and_prediction(self, y: np.ndarray) -> tuple[float, float]:
+        prediction = float(np.average(y))
+        sum_squared_residuals = 0
+        for observed in y:
+            sum_squared_residuals = sum_squared_residuals + pow((observed - prediction), 2)
+        return sum_squared_residuals, prediction
+
 
 class ClassificationNode(DecisionTreeNode):
     """
@@ -91,11 +98,10 @@ class ClassificationNode(DecisionTreeNode):
     """
 
     _prediction: int
-    _distribution: list[float]
+    _distribution: np.ndarray
     _gini_impurity: float
-    _num_classes: int
 
-    def __int__(self, depth: int, samples: list[int], num_classes: int, y: np.ndarray) -> None:
+    def __int__(self, depth: int, samples: np.ndarray) -> None:
         """Initialize a <DecisionTreeNode> specifically to be used in Classification Decision Trees.
          <y> are the target variables for the observations of the samples at the indices determined by <samples>. <y> is
          used to determine <self._distribution> which represents the probability that the observations in this node
@@ -104,25 +110,36 @@ class ClassificationNode(DecisionTreeNode):
          is equal to 1 - (prob_class_0)^2 - (prob_class_1)^2 - etc.
         """
         super().__init__(depth, samples)
-        self._num_classes = num_classes
-        # Determine <self._distribution> and <self._gini_impurity> based on <y>
-        self._distribution = []
-        self._gini_impurity = 1
-        for classification in range(num_classes):
-            curr_class_probability = float((y == classification).sum() / y.size)
-            self._distribution.append(curr_class_probability)
-            self._gini_impurity = self._gini_impurity - pow(curr_class_probability, 2)
-        # Determine <self._prediction> of node based on <self._distribution>
-        self._prediction = self._distribution.index(max(self._distribution))
+        self._prediction = None
+        self._distribution = None
+        self._gini_impurity = None
 
     @property
     def prediction(self) -> int:
         return self._prediction
 
     @property
-    def probability_of_classes(self) -> list[float]:
+    def probability_of_classes(self) -> np.ndarray:
         return self._distribution
 
     @property
     def gini_impurity(self) -> float:
         return self._gini_impurity
+
+    def set_properties(self, gini_impurity: float, probability_of_classes: np.ndarray) -> None:
+        self._prediction = int(np.argmax(probability_of_classes))
+        self._gini_impurity = gini_impurity
+        self._distribution = probability_of_classes
+
+    def determine_gini_impurity_and_distribution(self, y: np.ndarray, num_classes: int) -> tuple[float, np.ndarray]:
+
+        # Determine <self._distribution> and <self._gini_impurity> based on <y>
+        temp_distribution = []
+        gini_impurity = 1
+        for classification in range(num_classes):
+            curr_class_probability = float((y == classification).sum() / y.size)
+            temp_distribution.append(curr_class_probability)
+            gini_impurity = gini_impurity - pow(curr_class_probability, 2)
+        distribution = np.array(temp_distribution)
+
+        return gini_impurity, distribution
